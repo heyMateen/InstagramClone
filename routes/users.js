81a9dd7be8dcef2dae71dc85b3a1c4981f4b7576
Mateen Haider <120988104+heyMateen@users.noreply.github.com>
2024-12-21 19:05:48 +0500

    changes in users.js

--- routes/users.js
+++ routes/users.js
@@ -1,48 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const plm = require("passport-local-mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/instainsta");
+mongoose.connect("mongodb://127.0.0.1:27017/insta-clone");
 
 const userSchema = mongoose.Schema({
   username: String,
   name: String,
   email: String,
   password: String,
-  picture: {
+  profileImage: {
     type: String,
-    default: "def.png"
+    default: "def.png",
   },
-  contact: String,
-  bio: String,
-  stories: [
+  // contact: String,
+  // bio: String,
+  // stories: [
+  //   {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: "story"
+  //   }
+  // ],
+  // saved: [
+  //   {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: "post"
+  //   }
+  // ],
+  posts: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "story" 
-    }
+      ref: "post",
+    },
   ],
-  saved: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "post" 
-    }
-  ],
-  posts: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "post" 
-  }],
-  followers: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "user" 
-    } 
-  ],
-  following: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "user" 
-    }
-  ]
-})
+  //   followers: [
+  //     {
+  //       type: mongoose.Schema.Types.ObjectId,
+  //       ref: "user"
+  //     }
+  //   ],
+  //   following: [
+  //     {
+  //       type: mongoose.Schema.Types.ObjectId,
+  //       ref: "user"
+  //     }
+  //   ]
+});
 
 userSchema.plugin(plm);
 
